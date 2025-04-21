3fa1ef3bcb6bf153c00af83fb9b0da22f22b1a36
Abdulaziz Sadi <57579000+kitindi@users.noreply.github.com>
2025-04-21 09:46:09 +0300

    created all product api endpoints

--- index.js
+++ index.js
@@ -23,20 +23,39 @@ app.get("/categories", async (req, res) => {
   }
 });
 
-// Get all products
+// GET : all products /products
 
 app.get("/products", async (req, res) => {
   try {
-    const results = await pool.query(
-      "SELECT product.name as Product_Name, product.price, category.name as Category FROM product INNER JOIN category ON category.id = product.category_id"
-    );
+    // const results = await pool.query(
+    //   "SELECT product.name as Product_Name, product.price, category.name as Category FROM product INNER JOIN category ON category.id = product.category_id"
+    // );
+
+    const results = await pool.query("SELECT * FROM product");
+    if (results.rows.length === 0) {
+      return res.status(204).json({ message: "No products found" });
+    }
     return res.status(200).json(results.rows);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
 });
 
-// Create new product
+// GET /products/:id
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query("SELECT * FROM product WHERE id = $1", [id]);
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    return res.status(200).json(result.rows[0]);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+// POST : Create new product /products
 
 app.post("/products", async (req, res) => {
   const { name, description, price, quantity, category_id } = req.body;
@@ -68,6 +87,48 @@ app.post("/products", async (req, res) => {
     return res.status(500).json({ message: error.message });
   }
 });
+
+// PUT : Update product /products/:id
+
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, description, price, quantity, category_id } = req.body;
+  try {
+    const result = await pool.query("UPDATE product SET name = $1, description = $2, price = $3, quantity = $4, category_id = $5 WHERE id = $6 RETURNING *", [
+      name,
+      description,
+      price,
+      quantity,
+      category_id,
+      id,
+    ]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    return res.status(200).json(result.rows[0]);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+// DELETE : Delete product /products/:id
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query("DELETE FROM product WHERE id = $1 RETURNING *", [id]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    return res.status(200).json(result.rows[0]);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
 app.listen(port, () => {
   console.log(`first server is running on port ${port} at http://localhost:${port}`);
 });
